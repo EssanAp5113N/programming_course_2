5f1a08f1333371e263b1a343cc78feaabe3f7c10
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-02-15 14:58:57 +0700

    Выполнено задание 3.4 Лаб 8

--- labs/lab08_pointers_arrays/Lab08/src/main.cpp
+++ labs/lab08_pointers_arrays/Lab08/src/main.cpp
@@ -31,20 +31,33 @@ int main() {
     // Динамический массив
 
     int size = 10;
-    int *arrD1 = new int[size];
+    //int *arrD1 = new int[size];
 
-    for (int i = 0; i < size; i++) {
-        arrD1[i] = i * i;
-    }
+    //for (int i = 0; i < size; i++) {
+    //    arrD1[i] = i * i;
+    //}
 
-    for (int i = 0; i < size; i++) {
-        cout << arrD1[i] << endl;
-    }
+    //for (int i = 0; i < size; i++) {
+    //    cout << arrD1[i] << endl;
+    //}
 
-    delete[] arrD1;
+    //delete[] arrD1;
 
     // -----------------------------
 
+    int *arrD2 = new int[size];
+    int *p;
+    p = arrD2;
+
+    for (int i = 0; i < size; i++) {
+        *(p + i) = i * i;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << *(p + i) << endl;
+    }
+
+    delete[] arrD2;
 
     // Упражнение 4: линейный самоадресуемый список из 10 элементов.
     // TODO: сформируйте список и выведите элементы по порядку.
