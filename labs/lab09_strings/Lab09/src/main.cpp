62c1393c69511def034ab0310f4caa4d6654a5e2
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-02-23 11:39:05 +0700

    Выполнено Упражнение 4 СП Vector Лаб 9

--- labs/lab09_strings/Lab09/src/main.cpp
+++ labs/lab09_strings/Lab09/src/main.cpp
@@ -16,6 +16,31 @@ int main() {
 
     cout << "---------" << endl;
 
+        // Упражнение 4: ввод количества слов и самих слов.
 
+    int word_count = 0;
+
+    cin >> word_count;
+
+    if (word_count > 20 || word_count < 0) {
+        return 0;
+    }
+
+    // С помощью vector
+
+    vector<string> line(word_count);
+
+    for (int i = 0; i < word_count; i++) {
+        string l;
+        cin >> l;
+        line[i] = l;
+    }
+
+    cout << "---------" << endl;
+
+    for (int i = 1; i < word_count;) {
+        cout << line[i] << endl;
+        i += 2;
+    }
     return 0;
 }
