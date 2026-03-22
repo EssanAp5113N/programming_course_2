005d267e1086013da4644a8fa957594ff0af463f
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-03-22 11:08:08 +0700

    Выполено задание 2.1 Лаб 12

--- labs/lab12_combinatorics/Lab12/src/main.cpp
+++ labs/lab12_combinatorics/Lab12/src/main.cpp
@@ -1,36 +1,67 @@
 #include <iostream>
 using namespace std;
 
+int* Q;
+int* S;
+int* L;
+int* R;
 
-const int n = 3;
-int P[n]; 
-int R[n];
+int n, ch;
 
-void per(int k) {
-    for (int i = 0; i < n; i++) {
-        if (R[i] == 0) {
-            P[k] = i + 1;
-            R[i] = 1;
-            if (k == n - 1) {
-                for (int j = 0; j < n; j++) {
-                    cout << P[j] << " ";
-                }
-                cout << endl;
-            }
-            else {
-                per(k + 1);
-            }
-            R[i] = 0;
-        }
-    }
+
+void Queen(int j) {
+	for (int i = 0; i < n; i++) {
+		if (S[i] == 0 && R[j - i + n - 1] == 0 && L[j + i] == 0) {
+			S[i] = 1;
+			R[j - i + n - 1] = 1;
+			L[j + i] = 1;
+			Q[j] = i;
+			if (j == n - 1) {
+				ch++;
+				for (int k = 0; k < n; k++) {
+					cout << Q[k] + 1 << " ";
+				}
+				cout << endl;
+			}
+			else {
+				Queen(j + 1);
+			}
+			S[i] = 0;
+			R[j - i + n - 1] = 0;
+			L[j + i] = 0;
+		}
+	}
 }
 
-int main() {
-    for (int i = 0; i < n; i++) {
-        R[i] = 0;
-    }
-    
-    per(0);
 
-    return 0;
+void main() {
+	cin >> n;
+
+	S = new int[n];
+	Q = new int[n];
+	R = new int[2 * n - 1];
+	L = new int[2 * n - 1];
+
+	for (int i = 0; i < n; i++) {
+		S[i] = 0;
+	}
+
+	for (int i = 0; i < 2 * n - 1; i++) {
+		R[i] = 0;
+	}
+
+	for (int i = 0; i < 2 * n - 1; i++) {
+		L[i] = 0;
+	}
+
+
+	ch = 0;
+	Queen(0);
+
+	cout << "Всего решений: " << ch << endl;
+
+	delete[] S;
+	delete[] Q;
+	delete[] R;
+	delete[] L;
 }
