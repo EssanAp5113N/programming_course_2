57e8700d7b7746c6a5e3730cd0a026edea91d11d
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-03-28 19:23:19 +0700

    Преведение к ступ Лаб 13

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -2,16 +2,65 @@
 using namespace std;
 
 float eps = 0.000001;
-int systema(int n, float** A, float* X) {
-    return 0;
+int systema(int m, int n, float** A) {
+    int i, j, k, v;
+    float c;
+
+    for (i = 0; i < m; i++) {
+        v = i;
+        for (j = i + 1; j < m; j++) {
+            if (abs(A[j][i]) > abs(A[v][i])) {
+                v = j;
+            }
+        }
+
+        if (fabs(A[v][i]) < eps)
+            continue;
+
+        if (v != i) {
+            for (j = i; j <= n; j++) {
+                c = A[i][j];
+                A[i][j] = A[v][j];
+                A[v][j] = c;
+            }
+        }
+
+        c = A[i][i];
+        for (j = i; j <= n; j++)
+        {
+            A[i][j] = A[i][j] / c;
+        }
+
+        for (k = i + 1; k < m; k++)
+        {
+            c = A[k][i];
+            if (abs(c) > eps)
+            {
+                for (j = i; j <= n; j++)
+                {
+                    A[k][j] = A[k][j] - c * A[i][j];
+                }
+            }
+        }
+    }
+
+    if (fabs(A[m - 1][n - 1]) > eps)
+    {
+        c = A[m - 1][n - 1];
+        for (j = n - 1; j <= n; j++)
+        {
+            A[m - 1][j] = A[m - 1][j] / c;
+        }
+    }
+
+    return 1;
 }
 
 int main() {
     int i, j, g, n, m;
-    float **A, *X;
+    float **A;
     scanf_s("%d", &m);
     scanf_s("%d", &n);
-    X = new float[n];
     A = new float*[m];
     for (i = 0; i < m; i++) {
         A[i] = new float[n + 1];
@@ -19,18 +68,24 @@ int main() {
             scanf_s("%f", &A[i][j]);
         }
     }
-    for (i = 0; i < m; i++) {
-        for (j = 0; j <= n; j++) {
-            printf("%f", A[i][j]);
-        }
-    }
-    g = systema(n, A, X);
+    g = systema(m, n, A);
     if (g) {
-        for (i = 0; i < n; i++) {
-            printf("%8.3f", X[i]);
-        }
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+                    cout << A[i][j] << "\t";
+                }
+                cout << endl;
+            }
     }
     else {
         printf("ERROR\n");
     }
+
+    for (i = 0; i < m; i++) {
+        delete[] A[i];
+    }
+    delete[] A;
+   
 }
