26161cbd7b4f93764da056f4ebe3a2bfcc89f472
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-03-28 19:30:12 +0700

    Вывод и ввод через файлы Лаб 13

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <math.h>
 using namespace std;
 
 float eps = 0.000001;
@@ -59,29 +61,36 @@ int systema(int m, int n, float** A) {
 int main() {
     int i, j, g, n, m;
     float **A;
-    scanf_s("%d", &m);
-    scanf_s("%d", &n);
-    A = new float*[m];
+    ifstream fin("Test.txt");
+    ofstream fout("output.txt");
+
+    fin >> m >> n;
+    A = new float* [m];
+
     for (i = 0; i < m; i++) {
         A[i] = new float[n + 1];
         for (j = 0; j <= n; j++) {
-            scanf_s("%f", &A[i][j]);
+            fin >> A[i][j];
         }
     }
+    fin.close();
+
     g = systema(m, n, A);
     if (g) {
             for (i = 0; i < m; i++)
             {
                 for (j = 0; j <= n; j++)
                 {
-                    cout << A[i][j] << "\t";
+                    fout << A[i][j] << "\t";
                 }
-                cout << endl;
+                fout << endl;
             }
     }
     else {
         printf("ERROR\n");
     }
+
+    fout.close();
 
     for (i = 0; i < m; i++) {
         delete[] A[i];
