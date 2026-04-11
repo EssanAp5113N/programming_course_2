91c91b79ca7d9ceb2bcb51ed4cf6a4d4ab4290ed
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-04-11 13:31:57 +0700

    Выполнено задание

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,11 +1,64 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <string>
+#include <sstream>
 using namespace std;
 
-// Проверка создания ветки Повторная проверка
-
 float eps = 1e-9;
+
+bool parseEquation(const string& eq, float& a, float& b, float& c, float& d) {
+    string clean = "";
+    for (char ch : eq) {
+        if (!isspace(ch)) clean += ch;
+    }
+
+    size_t eqPos = clean.find('=');
+    string left = clean.substr(0, eqPos);
+    string right = clean.substr(eqPos + 1);
+
+    d = stof(right);
+    a = b = c = 0;
+    size_t i = 0;
+    float sign = 1.0;
+    float coeff = 1.0;
+
+    while (i < left.length()) {
+        if (left[i] == '+') {
+            sign = 1.0;
+            i++;
+        }
+        else if (left[i] == '-') {
+            sign = -1.0;
+            i++;
+        }
+        else if (i == 0) {
+            sign = 1.0;
+        }
+
+        coeff = 1.0;
+        if (i < left.length() && (isdigit(left[i]) || left[i] == '.')) {
+            size_t start = i;
+            while (i < left.length() && (isdigit(left[i]) || left[i] == '.')) i++;
+            coeff = stof(left.substr(start, i - start));
+        }
+
+        if (i < left.length() && left[i] == 'x') {
+            a += sign * coeff;
+            i++;
+        }
+        else if (i < left.length() && left[i] == 'y') {
+            b += sign * coeff;
+            i++;
+        }
+        else if (i < left.length() && left[i] == 'z') {
+            c += sign * coeff;
+            i++;
+        }
+    }
+    return true;
+}
+
 int systema(int m, int n, float** A, float* X, int* free_vars, int& free_count) {
     int i, j, k, v;
     float c;
@@ -67,6 +120,7 @@ int systema(int m, int n, float** A, float* X, int* free_vars, int& free_count) 
             }
         }
     }
+
     if (rank < n) {
         free_count = 0;
         for (j = 0; j < n; j++) {
@@ -100,7 +154,7 @@ int systema(int m, int n, float** A, float* X, int* free_vars, int& free_count) 
 
         delete[] basis_col;
         delete[] is_basis;
-        return 0;  
+        return 0;
     }
 
     for (i = n - 1; i >= 0; i--) {
@@ -114,54 +168,66 @@ int systema(int m, int n, float** A, float* X, int* free_vars, int& free_count) 
     for (i = 0; i < n; i++) {
         X[i] = A[i][n] / A[i][i];
     }
+
     delete[] basis_col;
     delete[] is_basis;
     return 1;
 }
 
 int main() {
-    int i, j, g, n, m;
-    float **A, *X;
+    int i, j, m = 3, n = 3;
+    float** A, * X;
+    string inputLine;
+
     ifstream fin("Test.txt");
     ofstream fout("output.txt");
 
-    fin >> m >> n;
+    getline(fin, inputLine);
+    fin.close();
+
+    stringstream ss(inputLine);
+    string equations[3];
+
+    for (i = 0; i < 3; i++) {
+        getline(ss, equations[i], ',');
+    }
+
+    float a[3], b[3], c[3], d[3];
+
+    for (i = 0; i < 3; i++) {
+        parseEquation(equations[i], a[i], b[i], c[i], d[i]);
+    }
+
+    A = new float* [m];
+    for (i = 0; i < m; i++) {
+        A[i] = new float[n + 1];
+        A[i][0] = a[i];
+        A[i][1] = b[i];
+        A[i][2] = c[i];
+        A[i][3] = d[i];
+    }
 
     X = new float[n];
-    A = new float* [m];
     int* free_vars = new int[n];
     int free_count = 0;
 
-    for (i = 0; i < m; i++) {
-        A[i] = new float[n + 1];
-        for (j = 0; j <= n; j++) {
-            fin >> A[i][j];
-        }
-    }
-    fin.close();
-
-    g = systema(m, n, A, X, free_vars, free_count);
+    int g = systema(m, n, A, X, free_vars, free_count);
 
     if (g == 1) {
-            for (i = 0; i < n; i++)
-            {
-                fout << X[i] << "\t";
-            }
-            fout << endl;
+        fout << X[0] << " " << X[1] << " " << X[2] << endl;
     }
     else if (g == -1) {
-        fout << "Система НЕСОВМЕСТНА" << endl;
+        fout << "Система НЕСОВМЕСТИМА" << endl;
     }
     else if (g == 0) {
-        for (i = 0; i < n; i++) {
-            fout << X[i] << "  ";
-        }
-        fout << endl;
+        fout << X[0] << " " << X[1] << " " << X[2] << endl;
         if (free_count > 0) {
             fout << "Свободные переменные: ";
             for (i = 0; i < free_count; i++) {
-                fout << "x" << free_vars[i] + 1;
-                if (i < free_count - 1) fout << ", ";
+                if (free_vars[i] == 0) fout << "x";
+                else if (free_vars[i] == 1) fout << "y";
+                else fout << "z";
+                if (i < free_count - 1) fout << " ";
             }
             fout << endl;
         }
@@ -175,4 +241,6 @@ int main() {
     delete[] A;
     delete[] X;
     delete[] free_vars;
+
+    return 0;
 }
