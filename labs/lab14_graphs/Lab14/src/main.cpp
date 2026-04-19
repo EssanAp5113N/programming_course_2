0841003796edff5d559d9eb65806c0ba91560167
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-04-19 17:58:04 +0700

    Работа с файлом Лаб 14

--- labs/lab14_graphs/Lab14/src/main.cpp
+++ labs/lab14_graphs/Lab14/src/main.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
     int** M, i, j, n, m, k;
-    scanf_s("%d", &n);
+    ifstream fin("Test.txt");
+    ofstream fout("Rez1.txt");
+
+    fin >> n;
     M = new int* [n];
     for (i = 0; i < n; i++)
     {
         M[i] = new int[n];
         for (j = 0; j < n; j++) M[i][j] = 0;
     }
-    scanf_s("%d", &m);
+    fin >> m;
     for (k = 0; k < m; k++)
     {
-        scanf_s("%d%d", &i, &j);
+        fin >> i >> j;
         M[i - 1][j - 1] = 1; M[j - 1][i - 1] = 1;
     }
 
-    printf("  ");
+    fin.close();
+    fout << "   ";
 
     for (i = 0; i < n; i++) {
-        printf("%d", i + 1);
-        printf("  ");
+        fout << i + 1;
+        fout << "  ";
     }
-    printf("\n");
+    fout << "\n";
 
     for (i = 0; i < n; i++) {
-        printf("%d", i + 1);
-        printf(" ");
+        fout << i + 1;
+        fout << "  ";
         for (j = 0; j < n; j++) {
-            printf("%d", M[i][j]);
-            printf("  ");
+            fout << M[i][j];
+            fout << "  ";
         }
-        printf("\n");
+        fout << "\n";
     }
+    fout.close();
     return 0;
 }
