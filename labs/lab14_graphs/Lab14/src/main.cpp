64fa963b5473154a55950cee0f7b084864b6d65d
Sabit <Sabit@LAPTOP-P64U4UQF>
2026-04-19 17:26:38 +0700

    Выполнено 1 Упр. в Задание 1 Лаб 14

--- labs/lab14_graphs/Lab14/src/main.cpp
+++ labs/lab14_graphs/Lab14/src/main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 
-// Lab 14
-// TODO: реализуйте решение по заданию в labs/lab14_graphs/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
 int main() {
-    // TODO
+    int** M, i, j, n, m, k;
+    scanf_s("%d", &n);
+    M = new int* [n];
+    for (i = 0; i < n; i++)
+    {
+        M[i] = new int[n];
+        for (j = 0; j < n; j++) M[i][j] = 0;
+    }
+    scanf_s("%d", &m);
+    for (k = 0; k < m; k++)
+    {
+        scanf_s("%d%d", &i, &j);
+        M[i - 1][j - 1] = 1; M[j - 1][i - 1] = 1;
+    }
+
+    printf("  ");
+
+    for (i = 0; i < n; i++) {
+        printf("%d", i + 1);
+        printf("  ");
+    }
+    printf("\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%d", i + 1);
+        printf(" ");
+        for (j = 0; j < n; j++) {
+            printf("%d", M[i][j]);
+            printf("  ");
+        }
+        printf("\n");
+    }
     return 0;
 }
